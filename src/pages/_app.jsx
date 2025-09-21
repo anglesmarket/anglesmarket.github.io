13461147b88e92b03fc0bfcc8c23f2e492db17b4
Your Name <you@example.com>
2025-09-21 05:05:31 +0330

    add footer

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,11 +1,14 @@
-
 import "../app/globals.css";
 import Article from "../app/components/layout/Article";
+import { Footer } from "../app/components/main/Footer";
 
 export default function MyApp({ Component, pageProps }) {
   return (
-    <Article>
-      <Component {...pageProps} />
-    </Article>
+    <>
+      <Article>
+        <Component {...pageProps} />
+      </Article>
+      <Footer />
+    </>
   );
 }
